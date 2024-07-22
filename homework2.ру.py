d509c10d9a202d2c5aed9f4f97ec85a4f0ddccc2
Alexei-52 <rod.leha2014@yandex.ru>
2024-07-22 20:46:26 +0300

    Add files via upload

---
+++ homework2.ру.py
@@ -0,0 +1,5 @@
+NumberOfHomework = "12"
+Number_of_hours_spent = "1.5"
+Course_name = "Python"
+Time_per_task = 12/1.5
+print(Course_name,"всего задач:",NumberOfHomework,",затрачено часов:",Number_of_hours_spent,",среднее время выполнения",Time_per_task,"часов" )
